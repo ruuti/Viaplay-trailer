07d8be8a952f98258679cdbe538b706460ada81d
ruuti <17232523+ruuti@users.noreply.github.com>
2021-04-23 23:38:40 +0200

    Helmet enabled and CORS allow * (#1)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 import "dotenv/config";
 import express from "express";
 import routes from "routes";
+import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
+
+// Enable CORS for all domains
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+// Enable Helmet
+app.use(helmet());
 
 // Add /trailer endpoints
 app.use('/trailer', routes.trailer);
