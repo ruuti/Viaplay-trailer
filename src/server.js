36d755aa81e050e602eb37368bfad9994f0307aa
ruuti <17232523+ruuti@users.noreply.github.com>
2021-04-24 11:15:26 +0200

    Rate limit API (#2)

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import routes from "routes";
 import helmet from "helmet";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
@@ -15,6 +16,21 @@ app.use(cors(corsOptions));
 
 // Enable Helmet
 app.use(helmet());
+
+// Enable trust proxy if service is running behind a reverse proxy
+const TRUST_PROXY = process.env.TRUST_PROXY || 0;
+app.set("trust proxy", TRUST_PROXY);
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  // limit each IP to 60 requests per windowMs or use value from env variables
+  max: process.env.MAX_REQUESTS_PER_MINUTE || 60,
+  message: {
+    "error": "Too many requests, please try again later."
+  }
+});
+// apply rate limiting to all requests
+app.use(limiter);
 
 // Add /trailer endpoints
 app.use('/trailer', routes.trailer);
